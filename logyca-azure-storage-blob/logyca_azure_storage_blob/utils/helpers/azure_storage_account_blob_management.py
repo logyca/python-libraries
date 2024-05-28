41a7fc2a32c6de3d8f41f970059348d4d6129422
logyca <tecnologiaeinformacion@logyca.com>
2024-05-27 20:32:47 -0500

    fix documentation

--- logyca-azure-storage-blob/logyca_azure_storage_blob/utils/helpers/azure_storage_account_blob_management.py
+++ logyca-azure-storage-blob/logyca_azure_storage_blob/utils/helpers/azure_storage_account_blob_management.py
@@ -630,7 +630,39 @@ class AzureStorageAccountBlobManagement:
         ## Examples
 
         ```Python
-        import os
+        from jaanca_chronometer import Chronometer # pip install jaanca-chronometer
+        from logyca_azure_storage_blob import AzureStorageAccountBlobManagement, SetCredentialsConnectionString
+
+        asabm=AzureStorageAccountBlobManagement(SetCredentialsConnectionString(connection_string=""))
+        chronometer=Chronometer()
+
+        # Example 1 - Small file
+        # Container root path
+        file='upload.txt'
+        chronometer.start()
+        url=asabm.container_blob_get_url_sas_read_to_download("blob_file","container_name",expiry_days=1)
+        chronometer.stop()
+        if url is False:
+            print("There is an error or the blob file does not exist")
+        else:
+            print(f"url: {url}")
+            print(f"Elapsed time: {chronometer.get_elapsed_time()}")
+
+        # Example 2 - Small file
+        # Container and subfolders path
+        container_folders=["folder1","folder2"]
+        file='upload.txt'
+        chronometer.start()
+        url=asabm.container_blob_get_url_sas_read_to_download("blob_file","container_name",container_folders,expiry_days=1)
+        chronometer.stop()
+        if url is False:
+            print("There is an error or the blob file does not exist")
+        else:
+            print(f"url: {url}")
+            print(f"Elapsed time: {chronometer.get_elapsed_time()}")
+
+
+
         ```
 
         # References
