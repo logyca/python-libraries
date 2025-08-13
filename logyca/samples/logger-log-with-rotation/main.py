e0ea777c91ad8d46cc39eba620499f398510fc3b
logyca <tecnologiaeinformacion@logyca.com>
2025-08-13 15:52:04 -0500

    update examples and logyca azure blob package feature

--- logyca/samples/logger-log-with-rotation/main.py
+++ logyca/samples/logger-log-with-rotation/main.py
@@ -5,6 +5,10 @@ CURRENT_ROOT_FOLDER=os.path.join(os.path.dirname(__file__),"logs")
 
 # Default parameters
 logger = Logger(log_dir=CURRENT_ROOT_FOLDER)
+# Custom parameters
+# log_messages_of_n_days_in_the_same_file = 1
+# day_files_to_keep=7
+# logger = Logger(log_dir=CURRENT_ROOT_FOLDER,interval=log_messages_of_n_days_in_the_same_file,backup_count=day_files_to_keep)
 
 # Custom parameters
 # logger = Logger(log_dir=CURRENT_ROOT_FOLDER,log_file_name="App_Test", is_enabled=os.getenv("logs_enabled",True), backup_count=7)
